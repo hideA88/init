9840fa0eb14758e89f44b60aeb8f3aad0b63051d
Hideaki Tarumi <h.tarumi@hozumi-studio.com>
2022-11-28 00:56:56 +0900

    guiとそれ以外とで必要になるものが違うのでファイル分割

---
+++ script/ubuntu/dev/npm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## npm lib
+npm install -g yarn
+npm install -g serverless
+npm install -g create-react-app
+npm install -g npm-check-updates
+npm install -g gitbook-cli
+npm install -g prettier
+npm install -g eslint
+npm install -g typescript
+
