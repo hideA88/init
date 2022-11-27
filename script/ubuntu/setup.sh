5192d1beb13a07b82387a995eb1046f1c6bd0855
Hideaki Tarumi <h.tarumi@hozumi-studio.com>
2022-11-28 03:06:12 +0900

    zshの初期設定のところをinstall-zshに移動

--- script/ubuntu/setup.sh
+++ script/ubuntu/setup.sh
@@ -14,20 +14,3 @@ else
     echo "already writed fs.inotify.max_user_watches=524288"
 fi
 
-# .zshrcのbaseファイルの作成
-if [ ! -f ~/.zshrc ]; then
-    curDir=`pwd`
-    cat ${curDir}/zshrc/base.zshrc > ~/.zshrc
-
-    ## 最終行を取得しておいて、あとで一番最後に追記
-    last=`tail -1 ~/.zshrc`
-    sed -i -e '$d' ~/.zshrc
-    
-    echo "iniDir=${curDir}" >> ~/.zshrc
-    echo -n -e "\n" >> ~/.zshrc #空行の追加
-    echo -n -e "\n" >> ~/.zshrc #空行の追加
-
-    echo ${last} >> ~/.zshrc
-else
-    echo "already exist ~/.zshrc"
-fi
