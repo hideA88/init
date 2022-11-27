7c590f85aea5cdf485e000446259f9c1b37f635e
Hideaki Tarumi <h.tarumi@hozumi-studio.com>
2022-11-28 01:00:26 +0900

    いったん各バージョンの取得処理について、最新をとってくる処理になるまでコメントアウトにし、
    envへの書き込み処理のところについても書いてなければ追記する形に変更

--- script/ubuntu/install-anyenv.sh
+++ script/ubuntu/install-anyenv.sh
@@ -1,51 +1,45 @@
 #!/bin/zsh
-NODE_VERSION=12.14.1
-RUBY_VERSION=2.6.4
-PYTHON_VERSION=3.8.1
-GO_VERSION=1.13.5
-
 
 if [ -z "`ls ~/.anyenv`" ]; then \
     git clone https://github.com/anyenv/anyenv ~/.anyenv
 fi
 
-#echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.zshrc
-eval "$(anyenv init -)"
+### zshrcに追記されていなければ追記する
+if [ -z "`grep "###\sfor\sanyenv" ~/.zshrc`" ]; then \
+    ## 最終行を取得しておいて、あとで最終行についき
+    last=`tail -1 ~/.zshrc`
+    sed -i -e '$d' ~/.zshrc
+
+    echo '### for anyenv' >> ~/.zshrc
+    echo 'source ${iniDir}/zshrc/anyenv.zshrc' >> ~/.zshrc
+    echo -n -e "\n" >> ~/.zshrc #空行の追加
+
+    echo ${last} >> ~/.zshrc
+    source ~/.zshrc
+else 
+    echo "already writing anyenv env"
+fi
 
 anyenv install --init
 
 anyenv install nodenv
-anyenv install pyenv
 anyenv install rbenv
 anyenv install goenv
-anyenv install tfenv
 
-source ~/.zshrc
+#source ~/.zshrc
 
-## install nodenv
-nodenv install ${NODE_VERSION}
-nodenv global ${NODE_VERSION}
-
-
-sudo mkdir /usr/local/opt
-sudo chown -R ${USER}:${USER} /usr/local/opt
-
-## install pyenv
-## これやらないとinstallコマンドが表示されない
-sudo apt install libffi-dev
-sudo ~/.anyenv/envs/pyenv/plugins/python-build/install.sh
-pyenv install ${PYTHON_VERSION}
-pyenv global  ${PYTHON_VERSION}
-
-## install rbenv
-sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
-rbenv install ${RUBY_VERSION}
-rbenv global ${RUBY_VERSION}
-
-## install goenv
-goenv install ${GO_VERSION}
-goenv global ${GO_VERSION}
-
-
-## install tfenv
-tfenv install
+### install nodenv
+#nodenv install ${NODE_VERSION}
+#nodenv global ${NODE_VERSION}
+#
+#sudo mkdir /usr/local/opt
+#sudo chown -R ${USER}:${USER} /usr/local/opt
+#
+### install rbenv
+#sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
+#rbenv install ${RUBY_VERSION}
+#rbenv global ${RUBY_VERSION}
+#
+### install goenv
+#goenv install ${GO_VERSION}
+#goenv global ${GO_VERSION}
