fb283bd47ac5191b26b84cbed2f791e3867ec64d
Hideaki Tarumi <h.tarumi@hozumi-studio.com>
2022-12-03 05:21:14 +0900

    何度も処理を実行できるように修正

--- install-script/bash/dev/cloud-cli.sh
+++ install-script/bash/dev/cloud-cli.sh
@@ -2,38 +2,84 @@
 
 CUR_DIR=`pwd`
 
-## awscli
-mkdir -p ~/tmp
-cd ~/tmp
+### awscli install
+function install_awscli() {
+  mkdir -p ./tmp
+  cd ./tmp
+  
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+  sudo ./aws/install $1 $2 $3 $4 $5
+  cd ${CUR_DIR}
+  sudo rm -rf ~/tmp
+}
 
-cd ${CUR_DIR}
-sudo rm -rf ~/tmp
-
-
-## awsenv
-mkdir -p ~/tmp
-cd ~/tmp
-
-AWSENV_VERSION=`curl -s https://api.github.com/repos/hideA88/awsenv/releases/latest| jq -r .tag_name`
-wget "https://github.com/hideA88/awsenv/releases/download/${AWSENV_VERSION}/awsenv"
-
-sudo chmod +x awsenv
-sudo cp awsenv /usr/local/bin
-
-cd ${CUR_DIR}
-sudo rm -rf ~/tmp
+which aws >/dev/null 2>&1
+if [  $? = 0 ]; then
+  echo "install aws cli"
+  install_awscli
+else
+  echo "update aws cli"
+  install_awscli --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+fi
 
 
-## gcloud
+### awsenv install
+LATEST_VERSION=`curl -s https://api.github.com/repos/hideA88/awsenv/releases/latest| jq -r .tag_name`
+
+function install_awsenv() {
+  echo "install awsenv"
+  mkdir -p ./tmp
+  cd ./tmp
+  wget "https://github.com/hideA88/awsenv/releases/download/${LATEST_VERSION}/awsenv"
+
+  sudo chmod +x awsenv
+  sudo cp awsenv /usr/local/bin
+
+  cd ${CUR_DIR}
+  sudo rm -rf ~/tmp
+}
+
+
+which awsenv >/dev/null 2>&1
+if [ $? = 0 ]; then
+  CUR_VERSION=`awsenv --version 2>&1 | head -1 | jq -r .msg | sed -e "s/version=//"`
+  if [ $LATEST_VERSION = $CUR_VERSION ]; then
+    # install skip
+    echo "awsenv latest version already installed"
+  else
+    CUR_PATH=`which awsenv`
+    sudo rm -rf $CUR_PATH
+    install_awsenv
+  fi
+else
+  install_awsenv
+fi
+
+### gcloud install
 sudo apt update 
 sudo apt install apt-transport-https ca-certificates gnupg 
 
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - 
-echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list 
+if [ ! -f /usr/share/keyrings/google-cloud-sdk.gpg ]; then
+    mkdir -p ./tmp
+    cd ./tmp
+
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg --output apt-key.gpg
+    gpg --no-default-keyring --keyring ./google-cloud-sdk.gpg --import apt-key.gpg
+    sudo cp ./google-cloud-sdk.gpg /usr/share/keyrings/
+
+    cd ${CUR_DIR}
+    rm -rf ./tmp
+else 
+    echo "aleady installed google-cloud-sdk repo keyrings"
+fi
+
+if [ ! -f /etc/apt/sources.list.d/google-cloud-sdk.list ]; then
+    echo "deb [signed-by=/usr/share/keyrings/google-cloud-sdk.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list 
+else
+    echo "already added google-cloud-sdk repository"
+fi
 
 sudo apt update  
 sudo apt install google-cloud-sdk 
