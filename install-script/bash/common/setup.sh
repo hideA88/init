b6ea2199b5a8ae99a6cfeb025a2255a36a52ad02
Hideaki Tarumi <hideaki.tarumi@timee.co.jp>
2022-12-15 00:28:45 +0900

    setupスクリプトのmac対応

--- install-script/bash/common/setup.sh
+++ install-script/bash/common/setup.sh
@@ -1,42 +1,60 @@
 #!/bin/bash
 
-sudo apt install git
-sudo apt install curl
-sudo apt install jq
-sudo apt install unzip
-sudo apt install direnv
+${INSTALL_COMMAND} git
+${INSTALL_COMMAND} curl
+${INSTALL_COMMAND} jq
+${INSTALL_COMMAND} unzip
+${INSTALL_COMMAND} direnv
 
-# docker上のwebpackでエラーにならないようにするため
-if [[ "fs.inotify.max_user_watches=524288" !=  `tail -1 /etc/sysctl.conf` ]]; then
-    echo fs.inotify.max_user_watches=524288|sudo tee -a /etc/sysctl.conf && sudo sysctl -p;
+# dock$er上のwebpackでエラーにならないようにするため
+if [ ${OS} == "mac" ]; then
+  sudo sysctl -w kern.maxfiles=524288
+  sudo sysctl -w kern.maxfilesperproc=524288
 else
-    echo "already writed fs.inotify.max_user_watches=524288"
+  if [[ "fs.inotify.max_user_watches=524288" !=  `tail -1 /etc/sysctl.conf` ]]; then
+      echo fs.inotify.max_user_watches=524288|sudo tee -a /etc/sysctl.conf && sudo sysctl -p;
+  else
+      echo "already writed fs.inotify.max_user_watches=524288"
+  fi
 fi
 
 
-## install font
-sudo apt install fontconfig
 
-### Ricttyのインストール
-sudo apt install -y fonts-roboto fonts-noto fonts-ricty-diminished
+## install font
+${INSTALL_COMMAND} install fontconfig
+
+if [ ${OS} == "mac" ]; then
+  ### Ricttyのインストールは大変なので変わりにHakuGenをダウンロード
+  brew tap homebrew/cask-fonts
+  ${INSTALL_COMMAND} font-hackgen
+  ${INSTALL_COMMAND} font-hackgen-nerd
+else
+  ### Ricttyのインストール
+  sudo apt install -y fonts-roboto fonts-noto fonts-ricty-diminished
+fi
 
 ### SourceHanCodeJP(源ノ各ゴシック)のインストール
 ls ~/.fonts/SourceHanCodeJP* >/dev/null 2>&1
-if [ ! $? = 0]; then
-    mkdir ./tmp
-    cd ./tmp
-    sourceZipUrl=`curl https://api.github.com/repos/adobe-fonts/source-han-code-jp/releases/latest | jq -r .zipball_url`
+if [ ! $? = 0 ]; then
+  mkdir ./tmp
+  cd ./tmp
+  sourceZipUrl=`curl https://api.github.com/repos/adobe-fonts/source-han-code-jp/releases/latest | jq -r .zipball_url`
 
-    curl -L  $sourceZipUrl --output ./source-han-code-jp.zip
-    unzip source-han-code-jp.zip
+  curl -L  $sourceZipUrl --output ./source-han-code-jp.zip
+  unzip source-han-code-jp.zip
 
-    mkdir ~/.fonts
+  mkdir ~/.fonts
+
+  if [ ${OS} == "mac" ]; then
+    cp ./*/*/*.otf ~/Library/Fonts
+  else
     cp ./*/*/*.otf ~/.fonts
-    rm -rf ./tmp
-    cd ..
-
     # fontのキャッシュクリア
     fc-cache -fv
+  fi
+
+  rm -rf ./tmp
+  cd ..
 else
     echo "already installed SourceHanCodeJP"
 fi
