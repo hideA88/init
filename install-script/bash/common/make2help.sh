c73a1a172eb82e6acf96dcd59c71493d001e0808
Hideaki Tarumi <h.tarumi@hozumi-studio.com>
2022-12-14 23:34:00 +0900

    make2helpのインストールをmac対応

--- install-script/bash/common/make2help.sh
+++ install-script/bash/common/make2help.sh
@@ -5,13 +5,13 @@ which make2help >/dev/null 2>&1
 if [ $? = 0 ]; then
     exit 0
 fi 
-sudo apt install jq
+${INSTALL_COMMAND} jq
 
 CUR_DIR=`pwd`
 
 ## install make2help
-mkdir -p ~/tmp
-cd ~/tmp
+mkdir -p ${CUR_DIR=}/tmp
+cd ${CUR_DIR=}/tmp
 
 ORG="Songmu"
 REPO="make2help"
@@ -19,13 +19,25 @@ REPO="make2help"
 LATEST_VERSION=`curl -s https://api.github.com/repos/${ORG}/${REPO}/releases/latest| jq -r .tag_name`
 echo $LATEST_VERSION
 
-TARGET="make2help_${LATEST_VERSION}_linux_amd64.tar.gz"
+
+if [ $OS == "ubuntu" ]; then
+  TARGET="make2help_${LATEST_VERSION}_darwin_arm64.zip"
+else
+  TARGET="make2help_${LATEST_VERSION}_linux_amd64.tar.gz"
+fi
+
 wget "https://github.com/${ORG}/${REPO}/releases/download/${LATEST_VERSION}/${TARGET}"
 
-sudo tar xzvf ${TARGET}
-cd "make2help_${LATEST_VERSION}_linux_amd64"
+if [ $OS == "ubuntu" ]; then
+  sudo unzip ${TARGET}
+  cd "make2help_${LATEST_VERSION}_darwin_arm64"
+else
+  sudo tar xzvf ${TARGET}
+  cd "make2help_${LATEST_VERSION}_linux_amd64"
+fi
+
 sudo chmod +x make2help 
 sudo cp make2help /usr/local/bin
 
 cd ${CUR_DIR}
-sudo rm -rf ~/tmp
+sudo rm -rf ${CUR_DIR=}/tmp
