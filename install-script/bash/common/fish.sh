435e724478c2c6e9685b576d9291d28516e01af4
Hideaki Tarumi <hideaki.tarumi@timee.co.jp>
2022-12-15 00:55:18 +0900

    install-fishのmac対応

--- install-script/bash/common/fish.sh
+++ install-script/bash/common/fish.sh
@@ -2,14 +2,31 @@
 
 which fish >/dev/null 2>&1
 if [ $? = 1 ]; then
-    echo "install fish"
+  echo "install fish"
+    
+  if [ ${OS} == "mac" ]; then
+    ${INSTALL_COMMAND} fish
+    which fish | sudo tee -a /etc/shells
+
+    ### Homebrew を使えるように
+    cmmd="set PATH /opt/homebrew/bin $PATH"
+    grep "$cmmd" ~/.config/fish/config.fish >/dev/null 2>&1
+    if [ ! $? = 0 ]; then 
+        echo "write to homebrew init setting to ~/.config/fish/config.fish"
+        echo "" >> ~/.config/fish/config.fish
+        echo "### for homebrew" >> ~/.config/fish/config.fish
+        echo $cmmd >> ~/.config/fish/config.fish
+        echo "" >> ~/.config/fish/config.fish
+    fi
+  else
     sudo apt-add-repository ppa:fish-shell/release-3
     sudo apt update
-    sudo apt install fish
+    ${INSTALL_COMMAND} fish
+  fi
 
-    # default shellをzshに変更
-    chsh -s $(which fish);
+  # default shellをzshに変更
+  chsh -s $(which fish);
 else
-    echo "update fish"
-    sudo apt install fish
+  echo "update fish"
+  ${UPDATE_COMMAND} fish
 fi
